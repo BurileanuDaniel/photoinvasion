cef05ad017f1a8bc344be1797194483a1e88f9e4
Daniel Burileanu <burileanu.daniel@gmail.com>
2014-03-22 19:15:05 +0200

    initial commit

---
+++ PhotoInvasion/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using PhotoInvasion.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoInvasion.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserProfileDbContext  _db = new UserProfileDbContext(); 
+
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var model = _db.UserProfiles.ToList();
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_db != null)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }    
+    }
+}
---
+++ PhotoInvasion/DAL/UserProfileDbContext.cs
@@ -0,0 +1,22 @@
+﻿using PhotoInvasion.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+
+namespace PhotoInvasion.DAL
+{
+    public class UserProfileDbContext : DbContext
+    {
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public UserProfileDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+    }
+}

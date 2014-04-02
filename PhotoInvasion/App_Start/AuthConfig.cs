98a54c5617f9f10434b7aef9a68ae10998fa9fdf
manunited777 <manu_bulboaca@yahoo.com>
2014-04-02 15:06:44 +0300

    twitter/microsoft login

--- PhotoInvasion/App_Start/AuthConfig.cs
+++ PhotoInvasion/App_Start/AuthConfig.cs
@@ -14,13 +14,13 @@ namespace PhotoInvasion
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "000000004411504B",
+                clientSecret: "8FTR0MgbvTNjPePdzl2haYMnVY1fPETj");
 
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            OAuthWebSecurity.RegisterTwitterClient(
+               consumerKey: "pMsZJOmnELsmD7du7jBCuKhcf",
+               consumerSecret: "lmmlWU2wY64muu89sX9QadO8ODuRqzS17ZLWmoOafnO6hbkxHN");
 
             OAuthWebSecurity.RegisterFacebookClient(
                 appId: "1453983538170294",
